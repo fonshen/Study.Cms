6fdcbe1d6354e58d78a92fb0e6bb8cd3731dba3f
fonshen <fonshen@hotmail.com>
2019-01-27 21:10:56 +0800

    新增学习代码

---
+++ Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content _contents;
+        public ContentController(IOptions<Content> option)
+        {
+            _contents = option.Value;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var contents = new List<Content>();
+            contents.Add(_contents);
+            for(int i=1;i<11;i++)
+            {
+                contents.Add(new Content { Id = i, Title = $"{i}的标题", Body = $"{i}的内容", Status = 1, Add_time = DateTime.Now.AddDays(-i) });
+            }
+            return View(new ContentViewModel { Contents = contents });
+        }
+    }
+}
---
+++ Sample01/Sample01/Models/Content.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Models
+{
+    public class Content
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime Add_time { get; set; }
+
+        public DateTime Modyfy_time { get; set; }
+    }
+}
